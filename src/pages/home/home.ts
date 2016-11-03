3ae7d9b101fdf4f55ec11d1a28ff384f8524863c
Lincoln Pires <me@lincolnpires.com>
2016-11-03 16:06:08 -0200

    new: login form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import firebase from 'firebase'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  currentUser: any;
+  authChecked: boolean = false;
+  credentials: {
+    email?: string;
+    password?: string;
+  };
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(
+    public navCtrl: NavController,
+    public ngZone: NgZone
+  ) {
+  }
+
+  ngOnInit() {
+    firebase.auth().onAuthStateChanged(currentUser => {
+      this.ngZone.run(() => {
+        if (currentUser) {
+          this.currentUser = currentUser;
+        } else {
+          this.currentUser = undefined;
+        }
+
+        this.authChecked = true;
+      });
+    });
+  }
+
+  doLogin(credentials): void {
+    if (credentials.valid) {
+      firebase.auth()
+        .signInWithEmailAndPassword(credentials.value.email, credentials.value.password)
+        .then(null, null);
+    }
+  }
+
+  doLogout() {
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+    }, (error) => {
+      console.log(error);
+    });
   }
 
 }
